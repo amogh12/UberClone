6c05358fbe036bd8940c7fdadda5987e4e5d4d01
amogh <amogh.s.phadke@gmail.com>
2018-05-05 23:33:06 -0600

    adding registration screen, font

--- app/src/main/java/com/example/asp/androiduberclone/MainActivity.java
+++ app/src/main/java/com/example/asp/androiduberclone/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.asp.androiduberclone;
 
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btnSignIn, btnRegister;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+                CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                                    .setDefaultFontPath("fonts/Arkhip_font.ttf")
+                                    .setFontAttrId(R.attr.fontPath).build());
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnRegister = (Button)findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRegisterDialog();
+            }
+        });
+    }
+
+    private void showRegisterDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Register ");
+        dialog.setMessage("Please use email to register");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View register_layout = inflater.inflate(R.layout.layout_register, null);
+
+        MaterialEditText edtEmail = register_layout.findViewById(R.id.editEmail);
     }
 }
--- readme.txt
+++ readme.txt
@@ -1,5 +1,3 @@
 https://www.youtube.com/watch?v=DvFPQBHfGzs&list=PLaoF-xhnnrRULoWAGjWJ79-BwD1mAMwB0
 
-time:15:09
-
-adding a shape
+27:45
